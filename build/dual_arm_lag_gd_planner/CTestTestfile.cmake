157bfaf912fda021d3a3740435b7bbd4784768a1
m11403602-byte <m11403602@gapps.ntust.edu.tw>
2026-07-10 04:16:47 +0800

    在這裡寫下您的提交訊息

---
+++ build/dual_arm_lag_gd_planner/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/angel/0710_code/src/0710_code/dual_arm_lag_gd_planner
+# Build directory: /home/angel/0710_code/build/dual_arm_lag_gd_planner
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/hiwin_dual_arm/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/angel/0710_code/install/hiwin_description/share/hiwin_description/package.sh"
